915dc81eb6b7dbdb4252457f812754bb576f1c37
Eduard Metzler <eduard.metzler@mail.ru>
2023-08-29 21:53:57 +0200

    added register

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -1,9 +1,11 @@
 import HomePage from "../pages/Home/Home.Page";
 import LoginPage from "../pages/Login/Login.Page";
+import RegistrPage from "../pages/Register/Register.Page";
 
 export const paths = {
   homePath: "/",
   loginPath: "/login",
+  registrPage: "/register",
   anyPath: "/*",
 };
 const AllRoutes = () => {
@@ -23,6 +25,13 @@ const AllRoutes = () => {
       isProtected: false,
       redirectPath: "/",
       id: "LoginPage",
+    },
+    {
+      path: paths.registrPage,
+      element: <RegistrPage />,
+      isProtected: false,
+      redirectPath: "/",
+      id: "RegistrPage",
     },
     {
       path: paths.anyPath,
