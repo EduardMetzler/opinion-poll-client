7bd75c451e8e9c45799656781dcfc90073c047da
Eduard Metzler <eduard.metzler@mail.ru>
2023-09-05 21:38:33 +0200

    added DashboardPage

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -2,12 +2,15 @@ import Error404Page from "../pages/Error404Page/Error404Page.Page";
 import HomePage from "../pages/Home/Home.Page";
 import LoginPage from "../pages/Login/Login.Page";
 import RegistrPage from "../pages/Register/Register.Page";
+import DashboardPage from "../pages/Dashboard/Dashboard.page";
+
 import { useUserStore } from "../stores/useUserStore";
 
 export const paths = {
   homePath: "/",
   loginPath: "/login",
   registrPage: "/register",
+  dashboardPage: "/dashboard",
   Error404Page: "/*",
 };
 const AllRoutes = () => {
@@ -28,15 +31,22 @@ const AllRoutes = () => {
       path: paths.loginPath,
       element: <LoginPage />,
       isProtected: user.id ? true : false,
-      redirectPath: "/",
+      redirectPath: "/dashboard",
       id: "LoginPage",
     },
     {
       path: paths.registrPage,
       element: <RegistrPage />,
       isProtected: user.id ? true : false,
-      redirectPath: "/",
+      redirectPath: "/dashboard",
       id: "RegistrPage",
+    },
+    {
+      path: paths.dashboardPage,
+      element: <DashboardPage />,
+      isProtected: !user.id ? true : false,
+      redirectPath: "/login",
+      id: "DashboardPage",
     },
     {
       path: paths.Error404Page,
