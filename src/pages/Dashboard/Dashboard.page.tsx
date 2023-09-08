8f0d5942f26f52037714fde8f91832bf5c476a21
Eduard Metzler <eduard.metzler@mail.ru>
2023-09-08 17:13:16 +0200

    added loading

--- src/pages/Dashboard/Dashboard.page.tsx
+++ src/pages/Dashboard/Dashboard.page.tsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
-import { paths } from "../../components/routes";
 import axios from "axios";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import Loading from "../../components/Loading";
+import { paths } from "../../components/routes";
 import { opinionPollStore } from "../../stores/opinionPoll";
+import { useState } from "react";
 
 const DashboardPage = () => {
+  const [loading, setLoading] = useState(true);
   const allMyOpinionPollsList = opinionPollStore(
     (state) => state.allMyOpinionPollsList
   );
@@ -16,11 +19,14 @@ const DashboardPage = () => {
       })
       .then((response) => {
         console.log(response);
+        setLoading(false);
 
         saveOpinionPoll(response.data);
+        console.log(allMyOpinionPollsList);
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
       });
   }, []);
   return (
@@ -32,17 +38,27 @@ const DashboardPage = () => {
       </Link>
       <div>
         {" "}
-        {allMyOpinionPollsList
-          ? allMyOpinionPollsList.map((opinionPoll) => {
-              return (
-                <Link to={opinionPoll.id} className="mx-2">
-                  <button className=" w-11/12 my-2  px-4 py-2 text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                    {opinionPoll.title}
-                  </button>
-                </Link>
-              );
-            })
-          : null}{" "}
+        {!loading ? (
+          <>
+            {allMyOpinionPollsList.length > 0 ? (
+              allMyOpinionPollsList.map((opinionPoll) => {
+                return (
+                  <Link to={opinionPoll.id} className="mx-2">
+                    <button className=" w-11/12 my-2  px-4 py-2 text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                      {opinionPoll.title}
+                    </button>
+                  </Link>
+                );
+              })
+            ) : (
+              <span className="flex  place-content-center ">
+                Bis jetzt noch nichts erstellt.
+              </span>
+            )}
+          </>
+        ) : (
+          <Loading />
+        )}
       </div>
     </>
   );
