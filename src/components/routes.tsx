88169ff6ac4fc1f91df158d8b8fcaa6d5d8417a0
Eduard Metzler <eduard.metzler@mail.ru>
2023-09-16 17:37:01 +0200

    updated routes

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -2,7 +2,7 @@ import Error404Page from "../pages/Error404Page/Error404Page.Page";
 import HomePage from "../pages/Home/Home.Page";
 import LoginPage from "../pages/Login/Login.Page";
 import RegistrPage from "../pages/Register/Register.Page";
-// import DashboardPage from "../pages/Dashboard/Dashboard.Page";
+import DashboardPage from "../pages/Dashboard/Dashboard.Page";
 import CreateOpinionPollPage from "../pages/CreateOpinionPoll/CreateOpinionPoll.Page";
 import MyOpinionPollPage from "../pages/MyOpinionPoll/MyOpinionPoll.Page";
 import PinionPollPage from "../pages/PinionPoll/PinionPoll.Page";
@@ -48,13 +48,13 @@ const AllRoutes = () => {
       redirectPath: "/dashboard",
       id: "RegistrPage",
     },
-    // {
-    //   path: paths.dashboardPage,
-    //   element: <DashboardPage />,
-    //   isProtected: !user.id ? true : false,
-    //   redirectPath: "/login",
-    //   id: "DashboardPage",
-    // },
+    {
+      path: paths.dashboardPage,
+      element: <DashboardPage />,
+      isProtected: !user.id ? true : false,
+      redirectPath: "/login",
+      id: "DashboardPage",
+    },
     {
       path: paths.createOpinionPoll,
       element: <CreateOpinionPollPage />,
